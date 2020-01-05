454f98844d1d6b0c5d2bd4829eeeb1109b7eeb82
Joy Reynolds <joyously@users.noreply.github.com>
2020-01-05 15:10:03 -0600

    for v2.6
    Put everything in the plugin.

--- shortcode-lister.php
+++ shortcode-lister.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: Shortcode Lister
+Plugin Name: Shortcode List
 Plugin URI: http://surpriseazwebservices.com/wordpress-plugins/shortcode-lister-wordpress-plugin/
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BTGBPYSDDUGVN
 Description: A plugin for the editor, to insert shortcodes available for use, with all their attributes.
-Version: 2.5
+Version: 2.6
 Author: Scott DeLuzio
 Author URI: https://scottdeluzio.com
 License: GPL2
@@ -31,8 +31,7 @@ if ( !defined( 'ABSPATH' ) ) {
 }
 
 
-add_action( 'media_buttons', 'shortcode_lister_menu', 15 );
-function shortcode_lister_menu( $editor_id ) {
+function shortcode_list_menu( $editor_id ) {
 	global $shortcode_tags;
 	
 	foreach ( array_keys( $shortcode_tags ) as $include ){
@@ -42,5 +41,40 @@ function shortcode_lister_menu( $editor_id ) {
 	echo $shortcodes_list;
 	echo '</select>';
 	
-	wp_enqueue_script( 'shortcode-select', plugins_url( 'js/shortcode-lister.js', __FILE__ ), '2.5', true );
+	wp_enqueue_script( 'shortcode-select', plugins_url( 'js/shortcode-list.js', __FILE__ ), '2.6', true );
+}
+add_action( 'media_buttons', 'shortcode_list_menu', 15 );
+
+/**
+ * Ajax handler for getting a shortcodes's attributes.
+ *
+ */
+function shortcode_list_get_shortcode_attributes() {
+	global $shortcode_tags;
+
+//	if ( current_user_can( 'edit_post' ) ) {
+		if ( empty( $_REQUEST['shortcode_tag'] ) ) {
+			wp_send_json_error( __( 'Missing shortcode tag.' ) );
+		}
+
+		$tag = wp_unslash( $_REQUEST['shortcode_tag'] );
+		if ( shortcode_exists( $tag ) ) {
+			if ( ! defined( 'SHORTCODE_LIST_SHORTCIRCUIT' ) ) {
+				define( 'SHORTCODE_LIST_SHORTCIRCUIT', true );
+				add_filter( "shortcode_atts_{$tag}", 'shortcode_list_atts_filter', 9, 4 );
+				call_user_func( $shortcode_tags[ $tag ], array(), '', $tag );
+			}
+		}
+//	}
+	wp_send_json_error();
+}
+add_action( 'wp_ajax_get-shortcode-attributes', 'shortcode_list_get_shortcode_attributes' );
+
+function shortcode_list_atts_filter( $out, $pairs, $atts, $shortcode ) {
+	if ( defined( 'SHORTCODE_LIST_SHORTCIRCUIT' ) && SHORTCODE_LIST_SHORTCIRCUIT ) {
+		wp_send_json_success( rtrim( trim( str_replace( 
+		array( '>', 'array (', ')', ",\n  '", "\n  '", "' = '", "' = " ), 
+		array( '', '', '', ' ', ' ', "='", '=' ),
+		var_export( $pairs, true ) ) ), ',' ) );
+	}
 }
